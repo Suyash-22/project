cd6920c18712a3a6c7194d58985ae18524a168ff
Suyash Singhal <suyashsinghal22222@gmail.com>
2023-06-12 13:52:33 +0530

    added multiply

--- sum.java
+++ sum.java
@@ -3,7 +3,9 @@ public class Main{
  public static void main(String[] args){
     int a = 2;
     int b=4;
-    int sum = a+b;
+    int c =3;
+    int d=4;
+    int sum = a+b+c+d;
     System.out.println(sum);
  }
  }
